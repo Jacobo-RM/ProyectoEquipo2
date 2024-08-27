b1aaa2cd5aa0848912c37b4debcf680bbfefeb73
curso11 <curso11@nomail.com>
2024-08-27 10:56:33 +0200

    Pestañas del Home

---
+++ clinicaJPPCliente/src/components/EditarCliente.vue
@@ -0,0 +1,2 @@
+<template></template>
+dni, nombre, direccion, String: telefono
---
+++ clinicaJPPCliente/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/clientes">Lista de Clientes</router-link>
+      <router-link to="/veterinarios">Lista de Veterinarios</router-link>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabsComponent'
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  gap: 10px;
+}
+
+router-link {
+  padding: 10px;
+  text-decoration: none;
+  color: #42b983;
+}
+
+router-link-active {
+  font-weight: bold;
+  color: #35495e;
+}
+</style>

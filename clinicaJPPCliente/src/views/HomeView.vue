e1c0f371a1c43c4a772759331c55b7e7c9570cf9
pablosainz-cic <emailprueba@hotmail.com>
2024-08-28 08:55:59 +0200

    subida parcial

--- clinicaJPPCliente/src/views/HomeView.vue
+++ clinicaJPPCliente/src/views/HomeView.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <nav>
-      <router-link to="/clientes">Lista de Clientes</router-link>
-      <router-link to="/veterinarios">Lista de Veterinarios</router-link>
-    </nav>
-    <RouterView />
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'TabsComponent'
-}
-</script>
-
-<style>
-nav {
-  display: flex;
-  gap: 10px;
-}
-
-router-link {
-  padding: 10px;
-  text-decoration: none;
-  color: #42b983;
-}
-
-router-link-active {
-  font-weight: bold;
-  color: #35495e;
-=======
     <h1>Bienvenido a la Clínica Veterinaria</h1>
     <nav>
       <ul>
@@ -62,6 +32,6 @@ nav li {
 
 nav a:hover {
   text-decoration: underline;
->>>>>>> main
+
 }
 </style>

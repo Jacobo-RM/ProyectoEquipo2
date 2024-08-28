9a2f4a6c60bc54a8d7caa254cc8abab476ebd6ed
PabloPDV <you@example.com>
2024-08-28 11:30:44 +0200

    fix id Veterinario, nuevos endpoints para Veterinario. Funcionalidad eliminar Veterinario

--- backend/demo/src/main/java/com/clinicaPPJ/server/demo/Repository/VeterinarioRepository.java
+++ backend/demo/src/main/java/com/clinicaPPJ/server/demo/Repository/VeterinarioRepository.java
@@ -1,9 +1,10 @@
 package com.clinicaPPJ.server.demo.Repository;
 
-import com.clinicaPPJ.server.demo.Model.Veterinario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.clinicaPPJ.server.demo.Model.Veterinario;
+
 @Repository
-public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
+public interface VeterinarioRepository extends JpaRepository<Veterinario, String> {
 }
